61c57141b27612bf8e9ab98886a573ad0175085e
NightCreature <paulkruitz@gmail.com>
2015-01-27 22:53:59 +0100

    Correct merging of the input, added a clamp template and added value clamping in the input state

--- SpaceSim/InputState.cpp
+++ SpaceSim/InputState.cpp
@@ -1,5 +1,5 @@
 #include "InputState.h"
-
+#include "MathUtilityFunctions.h"
 
 #ifdef _DEBUG
 //-----------------------------------------------------------------------------
@@ -14,6 +14,7 @@ void InputState::printState() const
         MSG_TRACE_CHANNEL( "Input State", "Input state for action %s is value %f ", inputAction.getAction().getNameString().c_str(), inputAction.getValue());
     }
 }
+#endif
 
 //-----------------------------------------------------------------------------
 //! @brief   TODO enter a description
@@ -33,25 +34,15 @@ void InputState::mergeInputState(const InputState& input)
             if (standardInputAction.getAction().getType() == m_inputState[counter].getAction().getType())
             {
                 float currentValue = m_inputState[counter].getValue();
-                bool currentSign = currentValue >= 0.0f;
                 float inputValue = standardInputAction.getValue();
-                bool inputSign = inputValue >= 0.0f;
-                if (currentSign == inputSign)
-                {
-                    //same sign, use max value, values should be clamped to 1.0f max anyway
-                    currentValue = currentValue >= 0.0f ? (currentValue > inputValue ? currentValue : inputValue) : ( currentValue < inputValue ? currentValue : inputValue);
-                   
-                }
-                else
-                {
-                    //Different signs, take the difference, need to figure out which one is bigger or negative
-                    currentValue = currentValue >= 0.0f ? (inputValue - currentValue) : (currentValue - inputValue);
-                }
+                //Different signs, take the difference, need to figure out which one is bigger or negative
+                currentValue = currentValue + inputValue;
+                currentValue = math::clamp(currentValue, -1.0f, 1.0f);
 #ifdef _DEBUG
-                if (test)
-                {
-                    MSG_TRACE_CHANNEL("Input State", "During merging %s value will change to %f", standardInputAction.getAction().getNameString().c_str(), currentValue);
-                }
+            if (test)
+            {
+                MSG_TRACE_CHANNEL("Input State", "During merging %s value will change to %f", standardInputAction.getAction().getNameString().c_str(), currentValue);
+            }
 #endif
 
                 m_inputState[counter].setValue(currentValue);
@@ -65,4 +56,3 @@ void InputState::mergeInputState(const InputState& input)
     }
 }
 
-#endif
