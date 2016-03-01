8b034e5c1aff457c3422e7844a95eecef0366e84
NightCreature <paulkruitz@gmail.com>
2016-03-01 23:42:20 +0100

    Added uint8 and uint16, also made it so that types will define size_t Added some calculations for finding how much space is needed to make a ptr aligned to a particular boundary Added function to make sure we keep the object we allocate on an aligned boundary with respect to a allocation header

--- SpaceSim/Memory/Allocator.h
+++ SpaceSim/Memory/Allocator.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Types.h"
+
 #include <assert.h>
 #include <malloc.h>
 
--- SpaceSim/Memory/PointerArithmetic.h
+++ SpaceSim/Memory/PointerArithmetic.h
@@ -4,8 +4,35 @@
 
 namespace PointerMath
 {
+    inline size_t findAdjustmentForAligmentFromCurrentPtr(void* ptr, size_t alignment)
+    {
+        //                              This bit masks out anything in the original pointer and only keeps the bits that are not aligned, if this returns 0 its already aligned
+        size_t adjustment = alignment - (reinterpret_cast<uintptr_t>(ptr) & reinterpret_cast<uintptr_t>(alignment - 1)); //adjustment is how many bytes we need to add to the current pointer to make it alligned
+
+         return adjustment == alignment ? 0 : adjustment; //If we need to add a whole alignment the pointer is already aligned
+    }
+
     inline uintptr_t alignPointer(void* ptr, size_t alignment)
     {
         return reinterpret_cast<uintptr_t>(ptr) + (alignment - 1) & (~(alignment - 1)); //Add alignment to original address than make the address a multiple of the alignment
+    }
+
+    inline uintptr_t alignPointerWithSpaceForHeader(void* ptr, size_t alignment, size_t headerSize)
+    {
+        size_t adjustment = findAdjustmentForAligmentFromCurrentPtr(ptr, alignment);
+
+        //We can't fit the header to make it aligned so we need to move the pointer to the next aligned spot to fit in the header
+        if (adjustment < headerSize)
+        {
+            size_t neededSpace = headerSize - adjustment; //How much is left after adjustment is subtracted from the headersize
+
+            adjustment += alignment * (neededSpace / alignment); //If the headersize is mutiples of the alignment add enough to fit the header
+
+            if (neededSpace % alignment > 0)
+            {
+                adjustment += alignment; //We had a fractional division above, working in integers so add an additional alignment to make it all fit properly.
+            }
+        }
+        return adjustment;
     }
 }
