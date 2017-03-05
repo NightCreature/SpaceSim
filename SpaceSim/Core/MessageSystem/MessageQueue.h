83e171e28a2fbe10fa1a207d134fc5e45dc735c4
NightCreature <paulkruitz@gmail.com>
2017-03-06 00:57:01 +0100

    Fix in swap message queues so we actually swap the queues

--- SpaceSim/Core/MessageSystem/MessageQueue.h
+++ SpaceSim/Core/MessageSystem/MessageQueue.h
@@ -17,7 +17,7 @@ public:
     void addMessage(const Message& msg) { m_messages[m_numberOfMessages] = msg; ++m_numberOfMessages; }
     void reset() { m_numberOfMessages = 0; }
 
-    typedef std::array<Message, 1024> Messages;
+    typedef std::array<Message, 1024> Messages; //This cant be a straight up message because we will lose the internal data of the message
     const Messages& getMessages() const { return m_messages; }
     size_t numberOfMessagages() const { return m_numberOfMessages; }
 private:
@@ -42,7 +42,7 @@ public:
     {
         MessageSystem::MessageQueue* temp = m_renderQueue;
         m_renderQueue = m_updateQueue;
-        m_renderQueue = temp;
+        m_updateQueue = temp;
     }
 
 
