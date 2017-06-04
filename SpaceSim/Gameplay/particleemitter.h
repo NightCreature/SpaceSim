d95aaf4cf758f663b5bbcaf0f847cede9ba3055e
Paul Kruitz <Paulkruitz@gmail.com>
2017-06-05 01:51:25 +0200

    Destroyed a bunch of old particle system code and replaced it with more cache friendly and extensible particle system code. Emitters now use generators and updaters to determine how we emit particles and how we update them, this allows for new effects to be added much more easily.

--- SpaceSim/Gameplay/particleemitter.h
+++ SpaceSim/Gameplay/particleemitter.h
@@ -2,6 +2,7 @@
 #define PARTICLEEMITTER_H
 
 #include "Math/vector3.h"
+#include "Gameplay/particle.h"
 #include "Graphics/Model.h"
 #include "Graphics/color.h"
 #include <deque>
@@ -9,13 +10,20 @@
 namespace ParticleSystem
 {
 
-class Particle;
-class ParticleSystemManager;
-class ColorCurve {};
+class IGenerator
+{
+public:
+    virtual void generate(size_t start, size_t end, const ParticleData& particleData) = 0;
+};
+
+class IUpdater
+{
+public:
+    virtual void update(double elapsedTime, const ParticleData& particleData) = 0;
+};
 
 struct ParticleEmitterData
 {
-    ColorCurve m_colorCurve;
     Vector3 m_emitterDirection;
     Vector3 m_emitterPosition;
     size_t m_maxNumberOfParticles = 25;
@@ -30,79 +38,13 @@ class ParticleEmitterComponentBased
 {
 public:
 
-    void update(float elapsedTime);
+    void update(double elapsedTime);
 private:
     ParticleData m_particleData;
-    Generators m_generators;
-    Updaters m_updaters;
-};
+    std::vector<IGenerator*> m_generators;
+    std::vector<IUpdater*> m_updaters;
 
-class ParticleEmitter
-{
-public:
-    //enum EmitterType
-    //{
-    //	maincore,
-    //	normal,
-    //	system,
-    //};
-
-    ParticleEmitter(ParticleSystemManager* pfxManager);
-    ParticleEmitter(ParticleSystemManager* pfxManager, const Vector3& position, const Vector3& normal);
-    ParticleEmitter(ParticleSystemManager* pfxManager, const Vector3& position, const Model* emitter);
-    ~ParticleEmitter();
-
-    void cleanup() {}
-
-    void proceed(float elapsedtime);
-
-    bool isAlive() const { return m_alive; }
-    const Vector3& getPosition() const { return m_position; }
-    float getStartVelocity() const { return m_startvelocity; }
-    void setAlive(bool alive);
-    void setPosition(const Vector3& position);
-    void setStartVelocity(float startvelocity);
-    void setParticleLifetime(float lifetime) { m_lifetime = lifetime; }
-    void setParticleStartColor(const Color& initialcolor) { m_initialcolor = initialcolor; }
-    void setParticleKey1Color(const Color& key1) { m_key1 = key1; }
-    void setParticleKey2Color(const Color& key2) { m_key2 = key2; }
-    void setParticelEndColor(const Color& endcolor) { m_endcolor = endcolor; }
-    void setEmissionRate(float emissionrate) { m_emissionrate = emissionrate; }
-    void setMaxParticles(float maxparticles) { m_maxparticles = maxparticles; }
-    void setLifeTime(float lifetime) { m_emitterlifetime = lifetime; }
-    void setSpreadAngle(float angle) { m_spreadangle = angle; }
-    void setParticleSize(float size) { m_particlesize = size; }
-    void setParticleKey1Pos(float pos) { m_key1pos = pos; }
-    void setParticleKey2Pos(float pos) { m_key2pos = pos; }
-    //void setType(EmitterType type) {m_type = type;}
-    //EmitterType getType() const {return m_type;}
-
-    void draw();
-protected:
-private:
-    typedef std::deque<Particle* > ParticleBuffer;
-    typedef std::deque<Particle* >::iterator ParticleBufferIT;
-
-    ParticleBuffer m_particlebuffer;
-    Vector3 m_position;
-    Vector3 m_normal;
-    Color m_initialcolor;
-    Color m_key1;
-    Color m_key2;
-    Color m_endcolor;
-    ParticleSystemManager* m_pfxManager;
-    float m_startvelocity;
-    float m_maxparticles;
-    float m_emissionrate;
-    float m_lifetime;
-    float m_spreadangle;
-    float m_emitterlifetime;
-    float m_particlesize;
-    float m_key1pos;
-    float m_key2pos;
-    int m_numparticles;
-    int m_numemit;
-    bool m_alive;
+    float m_emmisionRate;
 };
 
 }
