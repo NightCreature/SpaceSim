2b4b4ddacce5aa6d4e09460c21dd27f7ed6eee7e
Paul Kruitz <paulkruitz@gmail.com>
2024-03-29 23:35:45 -0400

    Added a tool to create texture packs

---
+++ Tools/TexturePackEditor/TexturePackEditor/TexturePackEditor/TexturePack.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Xml;
+using System.IO;
+using System.Drawing.Text;
+using System.Security.Policy;
+
+namespace TexturePackEditor
+{
+    internal class TexturePack
+    {
+        public TexturePack() 
+        {
+            FileName = "";
+            m_texturePack = new ArrayList();
+        }
+
+        ~TexturePack() { }
+
+        public bool AddTexture(string texture)
+        {
+            if (m_texturePack.Contains(texture))
+            {
+                return false;
+            }
+
+            m_texturePack.Add(texture);
+
+            return true;
+        }
+
+        public bool RemoveTexture(string texture)
+        {
+            if (!m_texturePack.Contains(texture))
+            {
+                return false;
+            }
+
+            m_texturePack.Remove(texture);
+
+            return true;
+        }
+
+        public bool OpenTexturePack(string filePath)
+        {
+            m_texturePack.Clear();
+
+            using var reader = new StreamReader(filePath, true);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(reader);
+            FromXml(xmlDoc);
+
+            FileName = filePath;
+
+            return true;
+        }
+        public bool SaveTexturePack(string filePath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+
+            ToXml(ref xmlDoc);
+            
+            xmlDoc.Save(filePath);
+
+            FileName = filePath;
+
+            return true;
+        }
+
+        ///Format of file is
+        ///<TexturePack>
+        ///  <Texture file_name="" />
+        ///</TexturePack>
+        private void ToXml(ref XmlDocument document)
+        {
+            var packElement = document.CreateElement(TexturePackElementName);
+            document.AppendChild(packElement);
+
+            foreach(var filePath in m_texturePack)
+            {
+                var textureElement = document.CreateElement(TexturelementName);
+                var filePathAttribute = document.CreateAttribute(FilePathElementName);
+                filePathAttribute.Value = filePath.ToString();
+                textureElement.Attributes.Append(filePathAttribute);
+
+                packElement.AppendChild(textureElement);
+            }
+        }
+        private void FromXml(XmlDocument document)
+        {
+            foreach (System.Xml.XmlElement texturePackElement in document.GetElementsByTagName(TexturePackElementName))
+            {
+                foreach (System.Xml.XmlElement textureElement in texturePackElement.GetElementsByTagName(TexturelementName))
+                {
+                    var filePathAttribute = textureElement.GetAttribute(FilePathElementName);
+                    if (filePathAttribute != "")
+                    {
+                        m_texturePack.Add(filePathAttribute);
+                    }
+                }
+            }
+        }
+
+        public ArrayList TexturePackContent { get { return m_texturePack; } }
+
+        public String FileName { get; set; }
+        private ArrayList m_texturePack;
+
+        private const string TexturePackElementName = "TexturePack";
+        private const string TexturelementName = "Texture";
+        private const string FilePathElementName = "file_path";
+    }
+}
